64d7d4f4c650eb4be2649cd44e11b33d632113e8
이지수 (Ji soo Lee) <angeljess@naver.com>
2023-04-11 23:07:25 +0900

    스프링 부트와 JPA활용 24강 - 주문 검색 기능 개발 (동적 쿼리를 queryDSL로 바꾸는 것은 다음 강에서 진행)

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.repository;
 
+import java.util.List;
 import javax.persistence.EntityManager;
 import jpabook.jpashop.domain.Order;
 import lombok.RequiredArgsConstructor;
@@ -19,5 +20,13 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-    // public List<Order> findAll(OrderSearch orderSearch) {}
+    public List<Order> findAll(OrderSearch orderSearch) {
+        return em.createQuery("select o from Order o join o.member m"
+            + " where o.status = :status"
+            + " and m.name like :name", Order.class)
+            .setParameter("status", orderSearch.getOrderStatus())
+            .setParameter("name", orderSearch.getMemberName())
+            .setMaxResults(1000)
+            .getResultList();
+    }
 }
