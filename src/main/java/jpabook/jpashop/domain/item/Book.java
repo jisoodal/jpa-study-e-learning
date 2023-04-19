7e57c8faf155742d7bba8e3354aade6366fae999
이지수 (Ji soo Lee) <angeljess@naver.com>
2023-04-19 23:09:43 +0900

    스프링 부트와 JPA활용 28강 - 상품 등록 기능 개발

--- src/main/java/jpabook/jpashop/domain/item/Book.java
+++ src/main/java/jpabook/jpashop/domain/item/Book.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@DiscriminatorValue("B")
+@DiscriminatorValue("B") // 엔티티 저장 시 구분 컬럼에 입력할 값을 지정. 만약 책 엔티티를 저장하면 구분 컬럼인 dtype에 값 B가 저장된다.
 @Getter
 @Setter
 public class Book extends Item {
